8371b984e7db86595a3c15b83c42094a0facd76e
Aertes <mrxu1010@outlook.com>
2018-03-21 16:03:55 +0800

    ADMIN

--- AdminLTE-vue/src/main.js
+++ AdminLTE-vue/src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './Layout'
+import App from './App'
 import router from './router'
-import Jquery from './jquery'
-import Bootstrap from './bootstrap'
-import FastClick from './fastclick'
+// import './assets/dist/js/jquery.min.js'
+import './lib/jquery-vender.js'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
+import './assets/dist/js/adminlte.min.js'
+import './assets/dist/css/AdminLTE.min.css'
+import './assets/dist/css/skins/_all-skins.min.css'
 
 Vue.config.productionTip = false
 
@@ -13,8 +18,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  components: { App }
 })
-
-FastClick.attach(document.body);
